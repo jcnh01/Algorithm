1d72dcee85b79bd2d3995060b6ad8d3b9385e158
chanhee01 <102280807+chanhee01@users.noreply.github.com>
2024-03-20 21:05:11 +0900

    [Silver V] Title: 덩치, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/7568. 덩치/덩치.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+data  =[]
+result = []
+for i in range(n):
+  x, y = map(int, input().split())
+  data.append((x, y))
+
+for i in range(n):
+  num = 1
+  for j in range(n):
+    if data[i][0] < data[j][0] and data[i][1] < data[j][1]:
+      num += 1
+  result.append(num)
+
+for i in result:
+  print(i , end = " ")
