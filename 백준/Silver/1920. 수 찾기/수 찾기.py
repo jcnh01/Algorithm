a9d6669c1b37129451153cc141893e4e474ea966
chanhee01 <102280807+chanhee01@users.noreply.github.com>
2024-04-07 13:22:59 +0900

    [Silver IV] Title: 수 찾기, Time: 456 ms, Memory: 47416 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -0,0 +1,27 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+numbers = list(map(int, input().split()))
+
+M = int(input())
+targets = list(map(int, input().split()))
+
+numbers.sort()
+
+def find(st, en, target) :
+    if st == en :
+        if numbers[st] == target :
+            print(1)
+        else :
+            print(0)
+        return
+
+    mid = (st + en) // 2
+    if numbers[mid] < target :
+        find(mid+1, en, target)
+    else :
+        find(st, mid, target)
+
+for i in range(M) :
+    find(0, N-1, targets[i])
