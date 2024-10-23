62d0432569073b663307256faa56a28565314c06
jcnh01 <102280807+jcnh01@users.noreply.github.com>
2024-10-23 16:14:27 +0900

    [Gold IV] Title: 음식 평론가, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/1188. 음식 평론가/음식 평론가.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+
+def gcd(a, b) :
+    if a % b == 0:
+        return b
+    return gcd(b, a % b)
+
+print(M - gcd(N, M))
