dc1fd0c97227fb5ea9bb46d3a167a4f642190680
jcnh01 <102280807+jcnh01@users.noreply.github.com>
2024-10-26 22:46:12 +0900

    [Gold V] Title: 수 이어 쓰기 2, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/1790. 수 이어 쓰기 2/수 이어 쓰기 2.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+
+n, k = map(int, input().split())
+ans = 0
+num = 1
+nine = 9
+
+while k > num * nine:
+    k -= (num * nine)
+    ans += nine
+
+    num += 1
+    nine = nine * 10
+
+ans = (ans + 1) + (k - 1) // num
+
+if ans > n: 
+    print(-1)
+else:
+    print(str(ans)[(k-1) % num])
